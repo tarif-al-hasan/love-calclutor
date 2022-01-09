1b0b6e802d7792e7030b6aed436d5b86f63a40b5
Tarif Al Hasan <92237522+tarif-al-hasan@users.noreply.github.com>
2022-01-09 14:48:08 +0600

    Update main.js

--- love-calclutor/main.js
+++ love-calclutor/main.js
@@ -1,33 +1,39 @@
 const calcBtn = document.getElementById("caclBtn");
 
 const loveCalc = () => {
+  // input value
+  let yourName = document.getElementById("yourName").value;
+  let loverName = document.getElementById("yourLover").value;
   let result = document.getElementById("result");
   let randomNbr = Math.floor(Math.random() * 100);
   result.textContent = randomNbr + `%`;
   let text = document.getElementById("text");
   if (randomNbr >= 75) {
-    text.innerHTML = `Wow . I can hear the wedding bells toll!`;
+    text.innerHTML = `${yourName} and ${loverName} true love`;
   } else if (randomNbr >= 60) {
-    text.innerHTML = `Your love needs a little more love.`;
+    text.innerHTML = `${yourName} and ${loverName} the best lover`;
   } else if (randomNbr >= 50) {
-    text.innerHTML = `Your love is frozen. Let it go. Let it go..`;
+    text.innerHTML = `${yourName} and ${loverName} Your love is frozen. Let it go. Let it go..`;
   } else if (randomNbr > 50) {
-    text.innerText = `You need more love`;
+    text.innerText = ` ${yourName} and ${loverName} You need more love`;
   } else if (randomNbr == 80 || randomNbr <= 100) {
-    text.innerText = `You Need  lots more love  .. Please keep try`;
+    text.innerText = ` ${yourName} and ${loverName} You Need  lots more love  .. Please keep try`;
   } else {
     text.innerText = `Low Lover`;
   }
-
+};
+calcBtn.addEventListener("click", () => {
   // input files
   let yourName = document.getElementById("yourName").value;
   let loverName = document.getElementById("yourLover").value;
-  if (yourName == isNaN || yourName == "") {
-    alert("Please Enter Your Name");
-  } else if (loverName == isNaN || yourName == "") {
-    alert("please enter your lover name");
+
+  if (yourName == "" && loverName == "") {
+    alert("Please input Enter You name and your lover name");
+  } else if (loverName == "") {
+    alert("Enter Your Lover Name");
+  } else if (yourName == "") {
+    alert("Enter Your Lover Name");
+  } else {
+    loveCalc();
   }
-};
-calcBtn.addEventListener("click", () => {
-  loveCalc();
 });
